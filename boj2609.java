969a3962c06fdd8f97dc91ce473f80cc90e22dc5
leehw1011 <leehw1011@naver.com>
2023-06-28 15:24:09 +0900

    2609 최대공약수와 최소공배수

---
+++ boj2609.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj2609 {
+
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        
+        int x,y, b;
+        int a = 1;
+
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+
+        for(int i=1;i<=x;i++){
+            if(x % i == 0 && y % i == 0){
+                a = i;
+            }
+        }
+        b = x * y / a;
+
+        bw.write(String.valueOf(a) + "\n");
+        bw.write(String.valueOf(b));
+
+        bw.flush();
+        bw.close();
+    }
+    
+}
